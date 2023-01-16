6f112322d306efb0255483ae07ccf1b1f8da5ecf
sureshdharisi <suresh.dharisi@gmail.com>
2023-01-16 08:09:27 -0500

    initial commit of rewards calculator

---
+++ rewards-calculator-parent/rewards-calculator-web/src/test/java/com/poc/rewards/calculator/RewardsCalculatorParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.rewards.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsCalculatorParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
